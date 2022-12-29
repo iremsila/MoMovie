d12d0c93e47cce64eee8f509696107241bc1cdd4
iremsila <83502239+iremsila@users.noreply.github.com>
2022-12-29 14:26:48 +0300

    Update text.dart

--- lib/utils/text.dart
+++ lib/utils/text.dart
@@ -2,10 +2,12 @@ import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 
 class modified_text extends StatelessWidget {
+  
   //const modified_text({Key? key}) : super(key: key);
+  
   final String text;
   final Color color;
-  final double size;
+  final double size; 
 
   modified_text({required this.text, required this.color, required this.size});
 
